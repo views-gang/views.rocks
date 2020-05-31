e3af30f08a6d79c9a451427119a8596023a6f17d
Vik Kotwizkiy <vik@ktwzk.me>
2020-06-01 01:48:06 +0500

    - sa-fond

--- sa/gen.py
+++
@@ -1,8 +0,0 @@
-import random
-n=random.randint(0, 180)
-b= f'''  -webkit-transform: rotate({n}deg);
-  -moz-transform: rotate({n}deg);
-  -ms-transform: rotate({n}deg);
-  -o-transform: rotate({n}deg);
-  transform: rotate({n}deg);'''
-print(b)
